4bcdd2fe32c54828faa5b1b1b51206138c71e607
Sergey Romanenko <70233726+purpln@users.noreply.github.com>
2023-01-16 06:06:31 +0300

    no message

--- Package.swift
+++ Package.swift
@@ -2,6 +2,13 @@
 
 import PackageDescription
 
+let loop: [Target.Dependency]
+#if os(Linux)
+loop = ["CEpoll"]
+#else
+loop = []
+#endif
+
 let package = Package(
     name: "app",
     products: [.executable(name: "app", targets: ["Application"])],
@@ -15,8 +22,14 @@ let package = Package(
         .target(name: "Architecture", dependencies: [
             .target(name: "Loop")
         ]),
-        .target(name: "Loop"),
+        .target(name: "Loop", dependencies: loop),
         .target(name: "File"),
         .target(name: "JSON"),
     ]
 )
+
+#if os(macOS) || os(iOS)
+package.platforms = [.macOS(.v13), .iOS(.v11)]
+#elseif os(Linux)
+package.targets.append(.systemLibrary(name: "CEpoll"))
+#endif
